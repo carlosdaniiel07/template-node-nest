4a97087231c850bafcef39a4cd2cd2fcf5ce11a4
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2021-01-01 15:15:29 -0300

    Melhoria GlobalErrorHandler

--- src/middlewares/global-error-handler.middleware.ts
+++ src/middlewares/global-error-handler.middleware.ts
@@ -1,13 +1,17 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from "@nestjs/common";
 import { Response } from "express";
 
 import { ApiException } from "src/models/api-exception.model";
 
 @Catch(ApiException)
 export class GlobalErrorHandler implements ExceptionFilter {
+  private readonly logger = new Logger(GlobalErrorHandler.name)
+  
   catch(exception: ApiException, host: ArgumentsHost) {
     const ctx = host.switchToHttp()
     const res = ctx.getResponse<Response>()
+
+    this.logger.error(`Exceção capturada: ${exception.message} - status: ${exception.getStatus()}`)
 
     return res.status(exception.getStatus()).json({ message: exception.message })
   }
